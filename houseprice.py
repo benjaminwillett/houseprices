51b18b6c5edc912addcacd30fa5e6309c9f1e230
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-04-11 15:21:51 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -3,7 +3,7 @@ from BeautifulSoup import BeautifulSoup
 import urllib3
 
 http = urllib3.PoolManager()
-url = http.request("https://www.realestate.com.au/neighbourhoods/cheltenham-3192-vic")
+url = http.request("GET", "https://www.realestate.com.au/neighbourhoods/cheltenham-3192-vic")
 content = url.read()
 
 soup = BeautifulSoup(content)
