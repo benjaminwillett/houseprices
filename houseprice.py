81988bf65e4ab07889a8ec8a2bd4c4b8faaadd90
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-04-11 14:56:37 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import requests
+from lxml import html
+
+r = requests.get("https://www.realestate.com.au/neighbourhoods/cheltenham-3192-vic")
+tree = html.fromstring(r.content)
+
+prices = tree.xpath('//*[@id="median-price"]/div[2]/div/div[1]/div[1]/div[1]/a[3]/div[2]')
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -10,3 +16,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
