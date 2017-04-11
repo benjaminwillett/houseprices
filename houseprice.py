25c59c68e5e7f59e73e6a9306604b4a7dbc11d80
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-04-11 20:31:17 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -11,7 +11,7 @@ url = http.request("GET", "https://www.realestate.com.au/neighbourhoods/cheltenh
 soup = BeautifulSoup(url)
 
 links = soup.findAll("div", {"class": "price strong"})
-temp = links[0]
+temp = links[1]
 print(str(temp) + "is temp")
 refined = temp
 print(str(refined) + "is refined")
