17b05dd5d30fed1e800d234f1ad889a0296496ab
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-04-11 22:02:36 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -66,12 +66,18 @@ def getContent():
 
 @app.route('/' , methods=['GET','POST'])
 def default():
+    global myThread
     global refinedChelt
     global refinedMent
     global refinedPark
     global refinedBeau
     global priceHistory
     global change
+
+    thread1 = myThread(1,"Thread-1", 1)
+    # Start new Threads
+    thread1.start()
+
     return render_template('main.html', REFINEDCHELT=refinedChelt, REFINEDMENT=refinedMent, REFINEDPARK=refinedPark, REFINEDBEAU=refinedBeau, PRICEHISTORY=priceHistory, CHANGE=change)
 
 
