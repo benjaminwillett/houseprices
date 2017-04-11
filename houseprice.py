3aca39204e19ada6bb21b525d99b9b54fea3fd7b
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-04-11 16:48:29 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from BeautifulSoup import BeautifulSoup
 import urllib3
 
+app = Flask(__name__)
+
 http = urllib3.PoolManager()
 url = http.request("GET", "https://www.realestate.com.au/neighbourhoods/cheltenham-3192-vic")
 content = url.read()
@@ -11,7 +13,6 @@ soup = BeautifulSoup(content)
 links = soup.findAll('//*[@id="median-price"]/div[2]/div/div[1]/div[1]/div[1]/a[3]/div[2]')
 
 
-app = Flask(__name__)
 
 
 @app.route('/' , methods=['GET','POST'])
@@ -19,10 +20,6 @@ def default():
     global links
     return render_template('main.html', LINKS=links)
 
-@app.route('/')
-def hello_world():
-    global links
-    return '4 Bed Medium House Price Cheltenham =', links
 
 
 if __name__ == '__main__':
