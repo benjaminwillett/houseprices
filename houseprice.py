347dd5084be9e1ba072adf36bf2c0cbd6867703a
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-04-11 17:03:55 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -7,17 +7,19 @@ app = Flask(__name__)
 http = urllib3.PoolManager()
 url = http.request("GET", "https://www.realestate.com.au/neighbourhoods/cheltenham-3192-vic", preload_content=False)
 #content = url.read()
-
+print(url)
 soup = BeautifulSoup(url)
+print(soup)
 
 links = soup.findAll('//*[@id="median-price"]/div[2]/div/div[1]/div[1]/div[1]/a[3]/div[2]')
-
+print(links)
 
 
 @app.route('/' , methods=['GET','POST'])
 def default():
     global links
-    return render_template('main.html', LINKS=links)
+    global soup
+    return render_template('main.html', LINKS=links, SOUP=soup)
 
 
 
