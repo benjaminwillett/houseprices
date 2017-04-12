19248efbaf40fb435f558b855854d81e1a63e0e3
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-04-12 22:09:38 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -102,7 +102,7 @@ def add_pricing():
     global refinedMent
     global refinedPark
     global refinedBeau
-    return jsonify(result1 = refinedChelt[1])
+    return jsonify(result1 = refinedChelt[2])
 
 
 if __name__ == '__main__':
