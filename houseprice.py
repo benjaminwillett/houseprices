6d412739226b339551fe80c24722dd06bccf4085
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-04-12 21:17:09 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -93,9 +93,7 @@ def default():
     global refinedMent
     global refinedPark
     global refinedBeau
-    return render_template, jsonify('main.html', REFINEDCHELT=refinedChelt, REFINEDMENT=refinedMent, REFINEDPARK=refinedPark, REFINEDBEAU=refinedBeau, CHANGE=change)
-
-
+    return render_template('main.html')
 
 @app.route('/_update')
 def add_pricing():
