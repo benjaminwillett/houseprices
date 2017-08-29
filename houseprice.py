7187faf53bac60515ed0b691ec4e2409c02ac45a
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-08-29 10:39:22 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -44,6 +44,7 @@ def getContent():
                 print("starting replace method")
                 p = re.compile('<div class="price strong">$')
                 print("RE compile worked")
+                print(string)
                 print("now going to run replace p.sub against string. Content of string variable is " + string)
                 a = p.sub("$", string)
                 print("finished replace method")
@@ -113,6 +114,9 @@ def default():
             try:
                 print("starting replace method")
                 p = re.compile('<div class="price strong">$')
+                print("RE compile worked")
+                print(string)
+                print("now going to run replace p.sub against string. Content of string variable is " + string)
                 a = p.sub("$", string)
                 print("finished replace method")
                 print("inserting string to dictionary")
