fceacfda735bf60a048c62eba218aa4d99b44295
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-09-28 11:31:53 +1000

    make it better

--- houseprice.py
+++ houseprice.py
@@ -191,9 +191,10 @@ def default():
 
     metals = [{"Gold": {"AMOUNT": "10oz"}, "Silver": {"AMOUNT": "10oz"}, "Platinum": {"AMOUNT": "10oz"}}]
 
-    return render_template('main.html', TICKERS=tickers,CURRENCY=currency,
+    return render_template('main.html', TICKERS=tickers, CURRENCY=currency,
                            POSTCODE=postcode, DAYCOUNT=daycount, UTILITIES=utilities, INSURANCES=insurances,
-                           LOANS=loans, CC=cc, BANKAC=bankac, SUPER=super, SAVINGS=savings, METALS=metals, legend=legend)
+                           LOANS=loans, CC=cc, BANKAC=bankac, SUPER=super, SAVINGS=savings, METALS=metals,
+                           LEGEND=legend)
 
 
 
