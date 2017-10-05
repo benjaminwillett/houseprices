545811333c4bb5ccc67e84841ae48a0dd761bb12
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-10-05 12:20:12 +1100

    make it better

--- houseprice.py
+++ houseprice.py
@@ -225,9 +225,8 @@ def default():
     for each in tickers[0]:
         lastHol = tickers[0][each]["Last"]
         quantity = tickers[0][each]["quantity"]
-        print(type(lastHol) + " is lastHol")
-        holdVal = lastHol * quantity
-        print(type(holdVal) + " is holdVal")
+        print(type(lastHol))
+        print(type(quantity))
 
 
     utilities = [{"Electricity":
