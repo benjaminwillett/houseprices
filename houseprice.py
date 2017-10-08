de62ddd4aee6777e26acee8b568e527de9dd2aa9
[benwillett]git config --global user.email [benw@techcamp.com.au] <ben.willett@distortenterprises.com>
2017-10-08 15:32:06 +1100

    make it better

--- houseprice.py
+++ houseprice.py
@@ -1,12 +1,73 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, Response, redirect, url_for, request, session, abort
 from BeautifulSoup import BeautifulSoup
 import urllib3
 import threading
 import json
-import os
-
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+# silly user model
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_secret"
+
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+# create some users with ids 1 to 20
+users = [User(id) for id in range(1, 21)]
+
+# somewhere to login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if password == username + "_secret":
+            id = username.split('user')[1]
+            user = User(id)
+            login_user(user)
+            return redirect(request.args.get("next"))
+        else:
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
+
+
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
+
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+
+
+# callback to reload the user object
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+
 
 http = urllib3.PoolManager()
 
@@ -59,6 +120,7 @@ letsthread()
 
 
 @app.route('/', methods=['GET', 'POST'])
+@login_required
 def default():
     letsthread()
 
@@ -351,8 +413,6 @@ def default():
                            LOANS=loans, CC=cc, BANKAC=bankac, SUPER=super, SAVINGS=savings, METALS=metals,
                            LEGEND=legend, THIRTYMA=thirtyMa, SIXTYMA=sixtyMa, NINETYMA=ninetyMa, TOTALVAL=totalval,
                            TOTALDOLLAR=totaldollar, AUSTOTAL=austotal)
-
-
 
 
 @app.route('/index_one' , methods=['GET', 'POST'])
